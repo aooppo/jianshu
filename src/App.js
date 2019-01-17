1c96e4e1d96a82a5e0cc1838d7d7e3fbaa96e8f0
TJ <i@voox.cc>
2019-01-17 17:28:10 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TodoItem from './TodoItem'
 
 class App extends Component {
+  state = {
+    list: [],
+    value: ''
+  }
+  add = () => {
+
+    this.setState({
+      ...this.state,
+      list : [...this.state.list, this.state.value],
+      value: ''
+    })
+  }
+  inputChange = (e) => {
+    this.setState({
+      ...this.state,
+      value: e.target.value
+    })
+  }
+  itemOnClick = (index) => {
+    this.state.list.splice(index, 1)
+    this.setState({
+      ...this.state,
+    })
+  }
   render() {
+    const list = this.state.list
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <input value={this.state.value} onChange={this.inputChange}/><button style={{background: 'green', color: 'white'}} onClick={this.add}>Add</button>
+
+        Hello.
+        <ul>
+          {
+            list.map((s, i) => <TodoItem key={i} index={i} itemOnClick={this.itemOnClick}>{s}</TodoItem>)
+          }
+        </ul>
+      </React.Fragment>
     );
   }
 }
